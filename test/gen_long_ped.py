e4f340fac780a5515b59c9ab1cd51f09ed6885e9
cbkmephisto <cbkmephisto@gmail.com>
2017-02-22 13:56:37 -0600

    Providing easy test scripts

--- test/gen_long_ped.py
+++ test/gen_long_ped.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-n=10000
+n=100   # 1000000
 for i in range(1, n, 2):
-    print(i, i + 1, i + 2)
-    print(i+1, i+1+n, i+2+n)
+    print(i, i + 1, i + 2, sep=",")
+    print(i+1, i+1+n, i+2+n, sep=",")
