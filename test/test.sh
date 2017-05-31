0e5f2179d614e13475473a975e60486046263b5c
cbkmephisto <cbkmephisto@gmail.com>
2017-05-31 16:45:50 -0500

    Fixed path

--- test/test.sh
+++ test/test.sh
@@ -5,4 +5,4 @@ set -xue
 python3 gen_long_ped.py > long_ped.csv
 echo "1" > list
 
-time python3 ../pedRefiner.py list long_ped.csv out.long_ped.csv
+time python3 ../pedRefiner/pedRefiner.py list long_ped.csv out.long_ped.csv
